e3ca2337389536399974a5711bc289d84832edb8
Javier Guerra <javier@guerrag.com>
2013-10-31 01:49:22 -0500

    first allocation draft

--- plank.c
+++ plank.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "plank.h"
 
@@ -25,17 +26,27 @@ plank_t *plk_create (size_t size) {
 	plank_t *p = malloc(sizeof(plank_t) + size);
 	p->empty_head.next = 0;
 	p->empty_head.size = size;
+	memset(p->data, 0, sizeof(empty_node_t));
 	return p;
 }
 
 void* plk_alloc (plank_t *plk, size_t size) {
 	if (!plk)
 		return NULL;
-	empty_node_t *p = nodecast(plk, p->);
+	empty_node_t *p = &plk->empty_head;
 	while (p->size) {
+		empty_node_t *nxt = nodecast(plk, p->next);
 		if (p->size == size) {
-			
+			*p = *nxt;
+			return nxt;
+		} else if (p->size > size+sizeof(empty_node_t)) {
+			empty_node_t *newnode = nodecast(plk, p->next+size);
+			*newnode = *nxt;
+			p->next = p->next+size;
+			p->size = p->size-size;
+			return nxt;
 		}
+		p = nxt;
 	}
 	return NULL;
 }
